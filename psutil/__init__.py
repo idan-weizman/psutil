5a6e1af7170fe0b6f5d0a0791d254869f037601b
Idan Weizman <goten811@gmail.com>
2017-10-03 15:29:00 +0300

    added support for pgid and sid on linux

--- psutil/__init__.py
+++ psutil/__init__.py
@@ -785,6 +785,16 @@ class Process(object):
             """
             return self._proc.num_fds()
 
+    if LINUX:
+
+        def pgid(self):
+            """Return the process group ID of the process."""
+            return self._proc.pgid()
+
+        def sid(self):
+            """Return the session ID of the process."""
+            return self._proc.sid()
+
     # Linux, BSD and Windows only
     if hasattr(_psplatform.Process, "io_counters"):
 
